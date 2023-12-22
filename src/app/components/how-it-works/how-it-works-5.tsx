7fe4a0956c5ae59235617b1f91a9eed7db5a7d23
AniketBahukhandi <aniketbahukhandi@gmail.com>
2023-12-22 12:08:25 +0530

    errors

--- src/app/components/how-it-works/how-it-works-5.tsx
+++ src/app/components/how-it-works/how-it-works-5.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import { how_works_five } from "@/data/how-works-five";
 
-const HowItWorksFive = ({style_2}:{style_2?:boolean}) => {
+const HowItWorksFive = ({ style_2 }: { style_2?: boolean }) => {
   return (
-    <section className={`how-it-works-two position-relative ${style_2?'pt-160 xl-pt-130 lg-pt-100':'pt-130 xl-pt-110'}`}>
+    <section
+      className={`how-it-works-two position-relative ${
+        style_2 ? "pt-160 xl-pt-130 lg-pt-100" : "pt-130 xl-pt-110"
+      }`}
+    >
       <div className="container">
         <div className="title-one d-flex align-items-center justify-content-between text-center mb-45 lg-mb-20">
           <span className="line"></span>
-          <h2 className="fw-600 ps-3 pe-3 wow fadeInUp" data-wow-delay="0.3s">  It’s your turn to outshine </h2>
+          <h2 className="fw-600 ps-3 pe-3 wow fadeInUp" data-wow-delay="0.3s">
+            {" "}
+            It’s your turn to outshine{" "}
+          </h2>
           <span className="line"></span>
         </div>
 
@@ -16,9 +23,6 @@ const HowItWorksFive = ({style_2}:{style_2?:boolean}) => {
             {how_works_five.map((item) => (
               <div key={item.id} className="col-lg-4 col-md-6">
                 <div className="card-style-five text-center position-relative mt-25 pb-35 lg-pb-20 wow fadeInUp">
-                  <div className="numb fw-500 d-flex align-items-center justify-content-center m-auto">
-                    <span>0{item.id}</span>
-                  </div>
                   <div className="title fw-500 text-lg text-dark mt-25 mb-10">
                     {item.title}
                   </div>
--- src/app/components/how-it-works/how-it-works.tsx
+++ src/app/components/how-it-works/how-it-works.tsx
@@ -5,14 +5,15 @@ import shape_2 from "@/assets/images/shape/shape_07.svg";
 import shape_3 from "@/assets/images/shape/shape_07.svg";
 import { how_works_data } from "@/data/how-works-data";
 
-
-
 const HowItWorks = () => {
   return (
     <section className="how-it-works position-relative bg-color pt-110 lg-pt-80 pb-110 lg-pb-70">
       <div className="container">
-        <div className="title-one text-center mb-65 lg-mb-40 wow fadeInUp" data-wow-delay="0.3s">
-          <h2 className="text-white">
+        <div
+          className="title-one text-center mb-65 lg-mb-40 wow fadeInUp"
+          data-wow-delay="0.3s"
+        >
+          <h2 className="text-black">
             How it’s{" "}
             <span className="position-relative">
               work?{" "}
@@ -29,14 +30,16 @@ const HowItWorks = () => {
           {how_works_data.map((item) => (
             <div
               key={item.id}
-              className={`col-xxl-3 col-lg-4 col-md-6 ${item.id === 2 ? "m-auto" : ""}`}
+              className={`col-xxl-3 col-lg-4 col-md-6 ${
+                item.id === 2 ? "m-auto" : ""
+              }`}
             >
               <div className="card-style-two text-center mt-25 wow fadeInUp">
                 <div className="icon rounded-circle d-flex align-items-center justify-content-center m-auto">
                   <Image src={item.icon} alt="icon" className="lazy-img" />
                 </div>
-                <div className="title fw-500 text-white">{item.title}</div>
-                <p>{item.sub_title}</p>
+                <div className="title fw-500 text-black">{item.title}</div>
+                <p className="text-black">{item.sub_title}</p>
               </div>
             </div>
           ))}
