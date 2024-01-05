f35c7ed5dd64666780981da0abd6d46155a2f89b
AniketBahukhandi <aniketbahukhandi@gmail.com>
2024-01-05 11:23:19 +0530

    fixed 2

--- src/app/components/common/popup/apply-modal.tsx
+++ src/app/components/common/popup/apply-modal.tsx
@@ -3,7 +3,26 @@ import ApplyForm from "../../forms/apply-now";
 
 const ApplyModal = () => {
   // State to track the window width
+  const [windowWidth, setWindowWidth] = useState(() => {
+    if (typeof window !== "undefined") {
+      return window.innerWidth;
+    }
+    return 0; // Return a default width if window is not defined
+  });
 
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    // Set up event listener for window resize
+    window.addEventListener("resize", handleResize);
+
+    // Clean up
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
     <div
       className="modal fade"
@@ -31,42 +50,51 @@ const ApplyModal = () => {
                 marginBottom: "20px",
               }}
             >
-              <div style={{ flex: 1, marginLeft: "10px", marginRight: "10px" }}>
-                {/* Why Register With Us section */}
+              {windowWidth > 768 && (
                 <div
-                  style={{
-                    backgroundColor: "#f7f7f7",
-                    padding: "20px",
-                    borderRadius: "5px",
-                    margin: "20px 0",
-                    boxShadow: "0px 3px 6px rgba(0, 0, 0, 0.1)",
-                  }}
+                  style={{ flex: 1, marginLeft: "10px", marginRight: "10px" }}
                 >
-                  <div
-                    style={{
-                      marginBottom: "15px",
-                      fontWeight: "bold",
-                      fontSize: "18px",
-                    }}
-                  >
-                    Why Register With Us?
+                  {/* Why Register With Us section */}
+                  <div>
+                    <div
+                      style={{
+                        backgroundColor: "#f7f7f7",
+                        padding: "20px",
+                        borderRadius: "5px",
+                        margin: "20px 0",
+                        boxShadow: "0px 3px 6px rgba(0, 0, 0, 0.1)",
+                      }}
+                    >
+                      <div
+                        style={{
+                          marginBottom: "15px",
+                          fontWeight: "bold",
+                          fontSize: "18px",
+                        }}
+                      >
+                        Why Register With Us?
+                      </div>
+                      <ul
+                        style={{ listStyleType: "none", padding: 0, margin: 0 }}
+                      >
+                        <li style={{ marginBottom: "10px", fontSize: "16px" }}>
+                          * With totally online Admission Process
+                        </li>
+                        <li style={{ marginBottom: "10px", fontSize: "16px" }}>
+                          * Upto 50% discount on application forms
+                        </li>
+                        <li style={{ marginBottom: "10px", fontSize: "16px" }}>
+                          * Upload your admission documents and get paytm
+                          rewards
+                        </li>
+                        <li style={{ marginBottom: "10px", fontSize: "16px" }}>
+                          * You won't get unwanted calls from third parties
+                        </li>
+                      </ul>
+                    </div>
                   </div>
-                  <ul style={{ listStyleType: "none", padding: 0, margin: 0 }}>
-                    <li style={{ marginBottom: "10px", fontSize: "16px" }}>
-                      * With totally online Admission Process
-                    </li>
-                    <li style={{ marginBottom: "10px", fontSize: "16px" }}>
-                      * Upto 50% discount on application forms
-                    </li>
-                    <li style={{ marginBottom: "10px", fontSize: "16px" }}>
-                      * Upload your admission documents and get paytm rewards
-                    </li>
-                    <li style={{ marginBottom: "10px", fontSize: "16px" }}>
-                      * You won't get unwanted calls from third parties
-                    </li>
-                  </ul>
                 </div>
-              </div>
+              )}
 
               <div style={{ flex: 1, marginLeft: "10px", marginRight: "10px" }}>
                 <div className="form-wrapper m-auto">
