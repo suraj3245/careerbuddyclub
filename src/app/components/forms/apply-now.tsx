8e0336c2036b34c6e089f2f9a3e1b5f7477fc72e
AniketBahukhandi <aniketbahukhandi@gmail.com>
2024-02-09 19:40:48 +0530

    large

--- src/app/components/forms/apply-now.tsx
+++ src/app/components/forms/apply-now.tsx
@@ -8,7 +8,8 @@ import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useRouter } from "next/navigation";
-
+import Image from "next/image";
+import icon from "@/assets/images/icon/icon_60.svg";
 interface IOption {
   value: string;
   label: string;
@@ -23,9 +24,6 @@ type IFormData = {
   password: string;
   level: string;
   stream: string;
-  LeadCampaign: string;
-  LeadSource: string;
-  LeadChannel: string;
 };
 
 // schema
@@ -44,9 +42,6 @@ const ApplyForm = () => {
   const [showResend, setShowResend] = useState(false);
   const [levelOptions, setLevelOptions] = useState<IOption[]>([]);
   const [streamOptions, setStreamOptions] = useState<IOption[]>([]);
-  const [leadCampaign, setLeadCampaign] = useState("");
-  const [leadSource, setLeadSource] = useState("");
-  const [leadChannel, setLeadChannel] = useState("");
   const router = useRouter();
 
   const fetchLevelOptions = async () => {
@@ -88,31 +83,22 @@ const ApplyForm = () => {
       // Handle error, e.g., set some state to show an error message
     }
   };
+  useEffect(() => {
+    fetchStreamOptions(); // Fetch stream options when the component mounts
+  }, []);
+  useEffect(() => {
+    fetchLevelOptions();
+    // Fetch stream options when the component mounts
+  }, []);
 
   const {
     register,
     handleSubmit,
     formState: { errors },
     reset,
-    setValue,
     getValues,
   } = useForm<IFormData>({});
 
-  useEffect(() => {
-    fetchStreamOptions();
-    fetchLevelOptions();
-
-    const queryParams = new URLSearchParams(window.location.search);
-    const campaign = queryParams.get("utm_campaign") || "";
-    const source = queryParams.get("utm_source") || "";
-    const channel = queryParams.get("utm_medium") || "";
-    setLeadCampaign(campaign);
-    setLeadSource(source);
-    setLeadChannel(channel);
-    setValue("LeadCampaign", campaign);
-    setValue("LeadSource", source);
-    setValue("LeadChannel", channel);
-  }, []);
   const requestOTP = (data: {
     name: string;
     country_code: string;
@@ -165,24 +151,27 @@ const ApplyForm = () => {
   }, [isVerificationSent, countdown]);
   const onSubmit = (data: IFormData) => {
     // Destructure the required fields from data
-    const submissionData = {
-      ...data,
-      LeadCampaign: leadCampaign,
-      LeadSource: leadSource,
-      LeadChannel: leadChannel,
-    };
-    console.log("Form Data:", submissionData);
+    const {
+      name,
+      email,
+      mobile,
+      verificationCode: otp,
+      password,
+      level,
+      stream,
+    } = data;
+
+    console.log("Form Data:", data);
 
     // Set up the request options for axios
     const options = {
       method: "POST",
+      // url: '${process.env.REACT_APP_API_URL}students/register', // Replace with your API's URL
       url: "https://test.careerbuddyclub.com:8080/api/students/register", // Replace with your API's URL
       headers: {
         "Content-Type": "application/json",
       },
-      data: {
-        submissionData,
-      }, // Send only the required data
+      data: { name, email, mobile, otp, password, level, stream }, // Send only the required data
     };
 
     // Make the POST request using axios
@@ -191,7 +180,7 @@ const ApplyForm = () => {
       .then((response) => {
         // Handle the response here, e.g., notify the user of success
         localStorage.setItem("token", response.data.access_token);
-
+        localStorage.setItem("username", name);
         console.log("Registration successful", response.data);
         toast.success("Your Account is created ! please check your email. 🚀", {
           position: "top-left",
@@ -401,9 +390,7 @@ const ApplyForm = () => {
             </div>
           </div>
         </div>
-        <input type="hidden" {...register("LeadCampaign")} />
-        <input type="hidden" {...register("LeadSource")} />
-        <input type="hidden" {...register("LeadChannel")} />
+
         <div
           className="agreement-checkbox d-flex justify-content-between align-items-center"
           style={{ justifyContent: "center" }}
