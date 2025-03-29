df14bd40e140d85e576874fc3deedfa9be91363a
siddharth455 <sharmasiddharth269@gmail.com>
2025-03-29 17:52:38 +0530

    changes

--- src/layouts/footers/component/social-links.tsx
+++ src/layouts/footers/component/social-links.tsx
@@ -4,22 +4,22 @@ const SocialLinks = () => {
   return (
     <>
       <li>
-        <a href="#">
-          <i className="bi bi-whatsapp"></i>
+      <a href="https://wa.me/7456000100" target="_blank" rel="noopener noreferrer">
+  <i className="bi bi-whatsapp"></i>
+</a>
+      </li>
+      <li>
+        <a href="https://www.linkedin.com/company/careerbuddyclub/">
+          <i className="bi bi-linkedin"></i>
         </a>
       </li>
       <li>
-        <a href="#">
-          <i className="bi bi-dribbble"></i>
+        <a href="https://www.youtube.com/@careerbuddyclub">
+          <i className="bi bi-youtube"></i>
         </a>
       </li>
       <li>
-        <a href="#">
-          <i className="bi bi-google"></i>
-        </a>
-      </li>
-      <li>
-        <a href="#">
+        <a href="https://www.instagram.com/careerbuddyclub?igsh=MWQzOGp0YTB3M2Jhag==">
           <i className="bi bi-instagram"></i>
         </a>
       </li>
