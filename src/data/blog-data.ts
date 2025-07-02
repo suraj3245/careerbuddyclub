cb4652ec871f9e616150fb02a037734128f35a86
Santosh22 <kandarisantosh3@gmail.com>
2025-07-02 14:38:20 +0530

    updated sitemap.sml

--- src/data/blog-data.ts
+++ src/data/blog-data.ts
@@ -1,33 +1,14 @@
 import { IBlogDataType } from "@/types/blog-type";
-import blog_1 from "@/assets/images/blog/blog_img_01.jpg";
-import blog_2 from "@/assets/images/blog/blog_img_02.jpg";
+
 import blog_3 from "@/assets/images/blog/media1.jpg";
 import blog_4 from "@/assets/images/blog/media2.jpg";
 import blog_5 from "@/assets/images/blog/media3.jpg";
 import blog_6 from "@/assets/images/blog/media4.jpg";
-import blog_11 from "@/assets/images/blog/blog_img_11.jpg";
-import blog_12 from "@/assets/images/blog/blog_img_12.jpg";
-import blog_13 from "@/assets/images/blog/blog_img_13.jpg";
-import blog_14 from "@/assets/images/blog/blog_img_14.jpg";
-import blog_15 from "@/assets/images/blog/blog_img_15.jpg";
-import blog_16 from "@/assets/images/blog/blog_img_16.jpg";
-import blog_17 from "@/assets/images/blog/blog_img_17.jpg";
-import blog_grid_1 from "@/assets/images/blog/blog_img_18.jpg";
 import blog_grid_2 from "@/assets/images/blog/blog_img_19.jpg";
-import blog_grid_3 from "@/assets/images/blog/blog_img_20.jpg";
-import blog_grid_4 from "@/assets/images/blog/blog_img_21.jpg";
-import blog_grid_5 from "@/assets/images/blog/blog_img_22.jpg";
-import blog_grid_6 from "@/assets/images/blog/blog_img_23.jpg";
-import blog_full_1 from "@/assets/images/blog/blog_img_24.jpg";
 import blog_full_2 from "@/assets/images/blog/blog_img_25.jpg";
-import blog_full_3 from "@/assets/images/blog/blog_img_26.jpg";
-import blog_full_4 from "@/assets/images/blog/blog_img_27.jpg";
-import blog_full_5 from "@/assets/images/blog/blog_img_28.jpg";
-import blog_full_6 from "@/assets/images/blog/blog_img_29.jpg";
-import blog_full_7 from "@/assets/images/blog/blog_img_30.jpg";
-import blog_full_8 from "@/assets/images/blog/blog_img_31.jpg";
+
 const blog_data: IBlogDataType[] = [
-  // blog one
+  
   {
     id: 1,
     img: blog_3,
@@ -64,7 +45,7 @@ const blog_data: IBlogDataType[] = [
     desc: "Tomfoolery crikey bits and bobs brilliant bamboozled down the pub amongst brolly hanky panky, cack bonnet arse over tit burke bugger all mate bodge. cillum dolore eu fugiat pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Suspendisse interdum consectetur libero id faucibu nisl. Lacus vel facilisis volutpat est velit egestas.Tempus imperdiet nulla malesuada pellentesque elit eget gravida cum. Sit amet ris nullam eget felis. Enim praesent elementum facilisis leo. Ultricies leo integer.",
     blog: "blog-one",
   },
-  // blog two
+  
   {
     id: 4,
     img: blog_6,
@@ -78,112 +59,7 @@ const blog_data: IBlogDataType[] = [
     blog: "blog-one",
   },
 
-  // blog postbox
-  {
-    id: 21,
-    img: blog_11,
-    grid_img: blog_grid_1,
-    img_full: blog_full_1,
-    tags: ["Solution"],
-    title: "Print, publishing qui visual ux quis layout mockups.",
-    author: "Martin Cooley",
-    date: "12 MAY, 2023",
-    featured: true,
-    desc: "Tomfoolery crikey bits and bobs brilliant bamboozled down the pub amongst brolly hanky panky, cack bonnet arse over tit burke bugger all mate bodge. cillum dolore eu fugiat pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Suspendisse interdum consectetur libero id faucibu nisl. Lacus vel facilisis volutpat est velit egestas.Tempus imperdiet nulla malesuada pellentesque elit eget gravida cum. Sit amet ris nullam eget felis. Enim praesent elementum facilisis leo. Ultricies leo integer.",
-    blog: "blog-postbox",
-  },
-  {
-    id: 22,
-    img: blog_12,
-    grid_img: blog_grid_2,
-    img_full: blog_full_2,
-    tags: ["Business"],
-    title: "Medieval origins to the digital era,  everything there is to know.",
-    author: "John Smith",
-    date: "10 April, 2023",
-    featured: true,
-    desc: "Tomfoolery crikey bits and bobs brilliant bamboozled down the pub amongst brolly hanky panky, cack bonnet arse over tit burke bugger all mate bodge. cillum dolore eu fugiat pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Suspendisse interdum consectetur libero id faucibu nisl. Lacus vel facilisis volutpat est velit egestas.Tempus imperdiet nulla malesuada pellentesque elit eget gravida cum. Sit amet ris nullam eget felis. Enim praesent elementum facilisis leo. Ultricies leo integer.",
-    blog: "blog-postbox",
-  },
-  {
-    id: 23,
-    img: blog_13,
-    grid_img: blog_grid_3,
-    img_full: blog_full_3,
-    tags: ["Marketing"],
-    title:
-      "Quis nostrud exercitation ullamco laboris nisi ut aliquip commodo cons.",
-    author: "James J. Allen",
-    date: "5 May, 2023",
-    featured: true,
-    desc: "Tomfoolery crikey bits and bobs brilliant bamboozled down the pub amongst brolly hanky panky, cack bonnet arse over tit burke bugger all mate bodge. cillum dolore eu fugiat pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Suspendisse interdum consectetur libero id faucibu nisl. Lacus vel facilisis volutpat est velit egestas.Tempus imperdiet nulla malesuada pellentesque elit eget gravida cum. Sit amet ris nullam eget felis. Enim praesent elementum facilisis leo. Ultricies leo integer.",
-    blog: "blog-postbox",
-  },
-  {
-    id: 24,
-    img: blog_14,
-    grid_img: blog_grid_4,
-    img_full: blog_full_4,
-    tags: ["Developer"],
-    title: "Designer's Checklist for Every UX/UI Project.",
-    author: "Paul R. Bruns",
-    date: "15 March, 2023",
-    featured: true,
-    desc: "Tomfoolery crikey bits and bobs brilliant bamboozled down the pub amongst brolly hanky panky, cack bonnet arse over tit burke bugger all mate bodge. cillum dolore eu fugiat pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Suspendisse interdum consectetur libero id faucibu nisl. Lacus vel facilisis volutpat est velit egestas.Tempus imperdiet nulla malesuada pellentesque elit eget gravida cum. Sit amet ris nullam eget felis. Enim praesent elementum facilisis leo. Ultricies leo integer.",
-    blog: "blog-postbox",
-  },
-  {
-    id: 25,
-    img: blog_15,
-    grid_img: blog_grid_5,
-    img_full: blog_full_5,
-    tags: ["Business"],
-    title: "10 days quick challange forboost visitors.",
-    author: "Stacey J. Meikle",
-    date: "3 Feb, 2023",
-    featured: true,
-    desc: "Tomfoolery crikey bits and bobs brilliant bamboozled down the pub amongst brolly hanky panky, cack bonnet arse over tit burke bugger all mate bodge. cillum dolore eu fugiat pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Suspendisse interdum consectetur libero id faucibu nisl. Lacus vel facilisis volutpat est velit egestas.Tempus imperdiet nulla malesuada pellentesque elit eget gravida cum. Sit amet ris nullam eget felis. Enim praesent elementum facilisis leo. Ultricies leo integer.",
-    blog: "blog-postbox",
-  },
-  {
-    id: 26,
-    img: blog_16,
-    grid_img: blog_grid_6,
-    img_full: blog_full_6,
-    tags: ["Architecture"],
-    title: "Easy way to boost your business.",
-    author: "Shawn E. Palmer",
-    date: "5 Jan, 2023",
-    featured: true,
-    desc: "Tomfoolery crikey bits and bobs brilliant bamboozled down the pub amongst brolly hanky panky, cack bonnet arse over tit burke bugger all mate bodge. cillum dolore eu fugiat pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Suspendisse interdum consectetur libero id faucibu nisl. Lacus vel facilisis volutpat est velit egestas.Tempus imperdiet nulla malesuada pellentesque elit eget gravida cum. Sit amet ris nullam eget felis. Enim praesent elementum facilisis leo. Ultricies leo integer.",
-    blog: "blog-postbox",
-  },
-  {
-    id: 27,
-    img: blog_17,
-    img_full: blog_full_7,
-    tags: ["Development"],
-    title: "Introducing new tools for your design.",
-    author: "Corrie H. Cates",
-    date: "20 June, 2023",
-    featured: true,
-    desc: "Tomfoolery crikey bits and bobs brilliant bamboozled down the pub amongst brolly hanky panky, cack bonnet arse over tit burke bugger all mate bodge. cillum dolore eu fugiat pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Suspendisse interdum consectetur libero id faucibu nisl. Lacus vel facilisis volutpat est velit egestas.Tempus imperdiet nulla malesuada pellentesque elit eget gravida cum. Sit amet ris nullam eget felis. Enim praesent elementum facilisis leo. Ultricies leo integer.",
-    blog: "blog-postbox",
-    grid_img: blog_full_7
-  },
-  {
-    id: 28,
-    img: blog_15,
-    img_full: blog_full_8,
-    tags: ["Development"],
-    title: "Speaking remotely at WordCamp US.",
-    author: "Corrie H. Cates",
-    date: "25 June, 2023",
-    featured: true,
-    desc: "Tomfoolery crikey bits and bobs brilliant bamboozled down the pub amongst brolly hanky panky, cack bonnet arse over tit burke bugger all mate bodge. cillum dolore eu fugiat pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Suspendisse interdum consectetur libero id faucibu nisl. Lacus vel facilisis volutpat est velit egestas.Tempus imperdiet nulla malesuada pellentesque elit eget gravida cum. Sit amet ris nullam eget felis. Enim praesent elementum facilisis leo. Ultricies leo integer.",
-    blog: "blog-postbox",
-    grid_img: blog_full_8
-  },
+  
 ];
 
 export default blog_data;
